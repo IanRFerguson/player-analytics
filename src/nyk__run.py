fad9c9c54ec4b3c7a8cc7ee128df3f3e04203390
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-01-14 01:02:11 -0500

    testing flexibility

--- src/nyk__run.py
+++ src/nyk__run.py
@@ -5,17 +5,20 @@ from src.api import get_all_boxscore_data
 from src.utilities.cli import cli
 from src.utilities.logger import logger
 import os
+from config import RAW_BQ_DATASET
 
 #####
 
 
 @flow(log_prints=True)
-def run(bq: GoogleBigQuery, full_refresh: bool):
+def run(bq: GoogleBigQuery, full_refresh: bool, dataset: str = RAW_BQ_DATASET):
     # Get formatted date from yesterday
     today_ = datetime.date.today()
     yesterday_ = (today_ - datetime.timedelta(days=1)).strftime("%b %d, %Y").upper()
 
-    get_all_boxscore_data(bq=bq, full_refresh=full_refresh, target=yesterday_)
+    get_all_boxscore_data(
+        bq=bq, full_refresh=full_refresh, target=yesterday_, dataset=dataset
+    )
 
 
 #####
@@ -30,10 +33,15 @@ if __name__ == "__main__":
         logger.setLevel(level=10)
         logger.debug("Running in debug mode...")
 
-    # Serve Prefect workflow
-    run.serve(
-        name="nyk-player-data",
-        tags=["onboarding"],
-        parameters={"bq": bq, "full_refresh": args_.full_refresh},
-        interval=60,
-    )
+    if not args_.test:
+        logger.info("Running workflow in production...")
+        # Serve Prefect workflow
+        run.serve(
+            name="nyk-player-data",
+            tags=["onboarding"],
+            parameters={"bq": bq, "full_refresh": args_.full_refresh},
+            interval=60,
+        )
+    else:
+        logger.info("Running workflow in development...")
+        run(bq=bq, full_refresh=args_.full_refresh, dataset=f"{RAW_BQ_DATASET}__test")
--- src/utilities/cli.py
+++ src/utilities/cli.py
@@ -8,5 +8,6 @@ def cli():
 
     parser.add_argument("--full-refresh", "-f", action="store_true", default=False)
     parser.add_argument("--debug", "-d", action="store_true", default=False)
+    parser.add_argument("--test", action="store_true", default=False)
 
     return parser.parse_args()
