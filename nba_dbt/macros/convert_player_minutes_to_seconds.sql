e7e4f6861646efdc271eb3b0c6aeabf1684f50b1
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-01-15 20:54:27 -0500

    update macro

--- nba_dbt/macros/convert_player_minutes_to_seconds.sql
+++ nba_dbt/macros/convert_player_minutes_to_seconds.sql
@@ -1,3 +1,3 @@
 {%- macro convert_player_minutes_to_seconds(value, delimiter=":") -%}
-    CAST(SPLIT({{ value }}, '{{ delimiter }}')[0] AS INTEGER) + (CAST(SPLIT({{ value }}, '{{ delimiter }}')[1] AS INTEGER) / 60)
+    CAST(SPLIT({{ value }}, '{{ delimiter }}')[0] AS FLOAT64) + (CAST(SPLIT({{ value }}, '{{ delimiter }}')[1] AS FLOAT64) / 60)
 {%- endmacro -%}
