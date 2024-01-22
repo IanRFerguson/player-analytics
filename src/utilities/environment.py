7eb280e2f83580d588fbfbd9922b36e81873fcfa
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-01-21 22:30:04 -0500

    flexibly handle environment

--- src/utilities/environment.py
+++ src/utilities/environment.py
@@ -16,6 +16,23 @@ def set_dataset(testing: bool, default_dataset: str):
     return default_dataset
 
 
+def set_service_account_absolute_path():
+    """
+    Updates environment to point to absolute path for GCP
+    service accounts
+    """
+
+    # TODO - Would love to make this more dynamic
+    root_ = "../.."
+    path_ = os.path.abspath(os.path.join(root_, os.environ["GCP_CREDS"]))
+
+    if not os.path.exists(path_):
+        raise OSError(f"Can't update GCP creds after attempt - {path_}")
+
+    logger.debug(f"Updating GCP_CREDS: {path_}")
+    os.environ["GCP_CREDS"] = path_
+
+
 def set_environment():
     """
     Loads environment variables to work across mutliple
@@ -25,6 +42,7 @@ def set_environment():
     here_ = os.path.dirname(__file__)
     env_path = os.path.abspath(os.path.join(here_, "../../development.env"))
     load_dotenv(dotenv_path=env_path)
+    set_service_account_absolute_path()
 
 
 if __name__ == "__main__":
