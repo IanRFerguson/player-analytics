05324f2ba9ee1329060526198a104d939b586507
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-01-31 08:43:29 -0500

    wip

---
+++ infrastructure/cloud_run.tf
@@ -0,0 +1,17 @@
+resource "google_cloud_run_service" "production" {
+  name     = "player-analytics-service"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "docker.io/ianrichardferguson/nba-player-analytics"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
