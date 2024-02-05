2d96d3f9cc5dd4216099cd687124a096cd3bc896
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-02-04 22:38:29 -0500

    update cloud infrastructure

--- infrastructure/datasets.tf
+++ infrastructure/datasets.tf
@@ -3,18 +3,27 @@
 
 # Production dataset
 resource "google_bigquery_dataset" "prod_dataset" {
-    dataset_id = "raw__nyk_data"
-    friendly_name = "[PROD] Raw NYK Data"
-    description = "Output dataset for production ELT runs"
-    location = "US"
-    default_table_expiration_ms = null
+  dataset_id                  = "raw__nyk_data"
+  friendly_name               = "[PROD] Raw NYK Data"
+  description                 = "Output dataset for production ELT runs"
+  location                    = "US"
+  default_table_expiration_ms = null
 }
 
 # Testing dataset
 resource "google_bigquery_dataset" "dev_dataset" {
-    dataset_id = "raw__nyk_data__test"
-    friendly_name = "[DEV] Raw NYK Data"
-    description = "Output dataset for development ELT runs"
-    location = "US"
-    default_table_expiration_ms = null
+  dataset_id                  = "raw__nyk_data__test"
+  friendly_name               = "[DEV] Raw NYK Data"
+  description                 = "Output dataset for development ELT runs"
+  location                    = "US"
+  default_table_expiration_ms = null
+}
+
+# Config dataset
+resource "google_bigquery_dataset" "config_dataset" {
+  dataset_id                  = "config"
+  friendly_name               = "Configuration"
+  description                 = "Stores load configs"
+  location                    = "US"
+  default_table_expiration_ms = null
 }
--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -18,3 +18,8 @@ resource "google_bigquery_dataset_iam_policy" "dev_dataset" {
   dataset_id  = google_bigquery_dataset.dev_dataset.dataset_id
   policy_data = data.google_iam_policy.owner.policy_data
 }
+
+resource "google_bigquery_dataset_iam_policy" "config_dataset" {
+  dataset_id  = google_bigquery_dataset.dev_dataset.dataset_id
+  policy_data = data.google_iam_policy.owner.policy_data
+}
