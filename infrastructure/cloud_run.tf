ad3892b1d6e907d86d8570b2da5499741abc25fa
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-02-05 22:41:27 -0500

    consolidate auth terraform

--- infrastructure/cloud_run.tf
+++ infrastructure/cloud_run.tf
@@ -1,4 +1,4 @@
-resource "google_cloud_run_service" "production" {
+resource "google_cloud_run_service" "production_cloud_run" {
   name     = "player-analytics-service"
   location = "us-central1"
 
@@ -14,4 +14,23 @@ resource "google_cloud_run_service" "production" {
     percent         = 100
     latest_revision = true
   }
+}
+
+#####
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.production_cloud_run.location
+  project  = google_cloud_run_service.production_cloud_run.project
+  service  = google_cloud_run_service.production_cloud_run.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
--- infrastructure/datasets.tf
+++ infrastructure/datasets.tf
@@ -1,6 +1,25 @@
 # These datasets need to be created in your project prior to 
 # running the ELT pipeline
 
+locals {
+  json_data = jsondecode(file("${path.module}/local.json"))
+}
+
+#####
+
+data "google_iam_policy" "owner" {
+  binding {
+    role = "roles/bigquery.dataOwner"
+
+    members = [
+      "serviceAccount:${local.json_data.SERVICE_ACCOUNT}",
+      "user:${local.json_data.USER_EMAIL}"
+    ]
+  }
+}
+
+#####
+
 # Production dataset
 resource "google_bigquery_dataset" "prod_dataset" {
   dataset_id                  = "raw__nyk_data"
@@ -26,4 +45,21 @@ resource "google_bigquery_dataset" "config_dataset" {
   description                 = "Stores load configs"
   location                    = "US"
   default_table_expiration_ms = null
+}
+
+#####
+
+resource "google_bigquery_dataset_iam_policy" "prod_dataset" {
+  dataset_id  = google_bigquery_dataset.prod_dataset.dataset_id
+  policy_data = data.google_iam_policy.owner.policy_data
+}
+
+resource "google_bigquery_dataset_iam_policy" "dev_dataset" {
+  dataset_id  = google_bigquery_dataset.dev_dataset.dataset_id
+  policy_data = data.google_iam_policy.owner.policy_data
+}
+
+resource "google_bigquery_dataset_iam_policy" "config_dataset" {
+  dataset_id  = google_bigquery_dataset.dev_dataset.dataset_id
+  policy_data = data.google_iam_policy.owner.policy_data
 }
